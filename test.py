1028b438ff446f5674e02f8e28f098548a311a56
Rexpert <liang2003147@gmail.com>
2019-10-24 22:32:04 +0800

    Update Python: Produce map sequence

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ not1.any()                               # All values in (column 0 & 1) is 1
 df = df.iloc[:, 0:4]
 
 
-# 2. Month: Portugese -> English
+# 2. Month: Portugese -> Number
 month = list(range(1, 13))
 month = list(map(lambda x: str(x).zfill(2), month))
 foo = pd.DataFrame({"month": df.iloc[:, 2].unique(),
@@ -79,9 +79,9 @@ df["name"].value_counts()
 # Paraiba       = 239 * 2 = 478
 # The only duplication occured in Alagoas:
 d = df[df.name == "Alagoas"]
-d[d.duplicated()]
+duplicate = d[d.duplicated()].index
 
-df = df.drop(index=259, axis=1)
+df = df.drop(index=duplicate, axis=1)
 df["name"].value_counts()  # Check for deletion
 
 # 4. State: Replace with actual portugese name
@@ -98,12 +98,41 @@ state_por = state_por.iloc[index]
 
 df.name = [x for item in state_por for x in repeat(item, 239)]
 
-## Try plot map with 1 example
-# right = df.query("year == 2017 and month == '11'")[["name", "number"]]
-# plot_df = map_df.copy()
-# plot_df = pd.merge(plot_df, right, on = "name", how = "left")
+'''# Try plot map with 1 example
+right = df.query("year == 2017 and month == '11'")[["name", "number"]]
+plot_df = map_df.copy()
+plot_df = pd.merge(plot_df, right, on = "name", how = "left")
 
-# plot_df.plot(column = "number", cmap = "Purples", figsize = (10, 10), linewidth = 0.8, edgecolor = "0.8", vmin = 0, vmax = 900, legend = True, norm = plt.Normalize(vmin = 0, vmax = 900))
+fig = plot_df.plot(column = "number", cmap = "Purples", figsize = (10, 10), linewidth = 0.8, edgecolor = "0.8", vmin = 0, vmax = 900, legend = True, norm = plt.Normalize(vmin = 0, vmax = 900))
+
+## Legend with no upper bound
+# fig = plot_df.plot(column = "number", cmap = "Purples", figsize = (10, 10), linewidth = 0.8, edgecolor = "0.8", vmin = 0, vmax = 900, norm = plt.Normalize(vmin = 0, vmax = 900))
+# ax = plt.gca()
+# im = ax.imshow(np.arange(0,1000).reshape(10,100), cmap = "Purples")
+# plt.colorbar(im,fraction=0.046, pad=0.04)
+
+fig.axis("off")
+fig.set_title('Brazil Forest Fire', fontdict={'fontsize': '25', 'fontweight' : '3'})
+
+mon = int(str(201711)[4:])
+year = str(201711)[:4]
+eng_month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+annotation = eng_month[mon-1] + " " + year
+x_coord = 2 * (int(year) + mon/12) - 4071
+fig.annotate(annotation, xy=(.3, .4), xycoords='figure fraction', horizontalalignment='right',  verticalalignment ='top', fontsize=35)
+fig.plot(x_coord, -36, "o", color = "#69549E", markersize = 20, alpha = 0.5, clip_on=False, zorder = 100)
+fig.text(x_coord, -39, annotation, horizontalalignment = "center", fontsize = 15)
+
+plt.ylim(-35,7)
+plt.xlim(-75,-34)
+
+for n in range(1, 21) :
+    x_coord = -75 + (n - 1) * 2
+    color = '#69549E' if n % 2 else '#E8E7EF'
+    plt.hlines(-36, x_coord, x_coord + 2, colors=color, lw=5, clip_on = False, zorder = 100)
+
+plt.show()
+'''
 
 ## Plot multiple maps
 vmin = int(math.floor(min(df.number)/100))*100
@@ -119,24 +148,53 @@ wide_df = wide_df.pivot(index="name", columns="time", values="number")
 
 plot_df = pd.merge(map_df, wide_df, on="name", how="left")
 
-year_list = wide_df.columns[:3]
+year_list = wide_df.columns
 
 eng_month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-for year in year_list :
-    fig = plot_df.plot(column = year, cmap = "Purples", figsize = (10, 10), linewidth = 0.8, edgecolor = "0.8", vmin = vmin, vmax = vmax, legend = True, norm = plt.Normalize(vmin = vmin, vmax = vmax))
+for now in year_list :
+    fig = plot_df.plot(column = now, cmap = "Purples", figsize = (10, 10), linewidth = 0.8, edgecolor = "0.8", vmin = vmin, vmax = vmax, legend = True, norm = plt.Normalize(vmin = vmin, vmax = vmax))
 
     fig.axis("off")
     fig.set_title('Brazil Forest Fire', fontdict={'fontsize': '25', 'fontweight' : '3'})
 
-    mon = int(str(year)[5:])
-    annotation = eng_month[mon-1] + " " + str(year)[:4]
-    fig.annotate(annotation, xy=(0.1, .225), xycoords='figure fraction', horizontalalignment='left',  verticalalignment ='top', fontsize=35)
+    mon = int(str(now)[4:])
+    year = str(now)[:4]
+    annotation = eng_month[mon-1] + " " + year
+    x_coord = 2 * (int(year) + mon/12) - 4071
+    fig.annotate(annotation, xy=(.3, .4), xycoords='figure fraction', horizontalalignment='right',  verticalalignment ='top', fontsize=35)
+    fig.plot(x_coord, -36, "o", color = "#69549E", markersize = 20, alpha = 0.5, clip_on=False, zorder = 100)
+    fig.text(x_coord, -39, annotation, horizontalalignment="center", fontsize=15)
 
-    filepath = os.path.join("maps", str(year)+'.png')
+    plt.ylim(-35,7)
+    plt.xlim(-75,-34)
+
+    for n in range(1, 21) :
+        x_coord = -75 + (n - 1) * 2
+        color = '#69549E' if n % 2 else '#E8E7EF'
+        plt.hlines(-36, x_coord, x_coord + 2, colors=color, lw=5, clip_on = False, zorder = 100)
+
+    filepath = os.path.join("maps", str(now)+'.png')
     chart = fig.get_figure()
     chart.savefig(filepath, dpi=300)
 
-    del fig
-    del chart
+    plt.close()
 
 print("done")
+
+
+
+from PIL import Image
+import glob
+ 
+# Create the frames
+frames = []
+imgs = glob.glob("maps/*.png")[:5]
+for i in imgs:
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+ 
+# Save into a GIF file that loops forever
+frames[0].save('png_to_gif.gif', format='GIF',
+               append_images=frames[1:],
+               save_all=True,
+               duration=150, loop=0)
